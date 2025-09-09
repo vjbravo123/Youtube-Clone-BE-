1a68049e663e8d67655805773875e78833e27418
vjbravo123 <vjbravojoshi@gmail.com>
2025-09-09 21:23:14 +0530

    add channel model

---
+++ models/Channel.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+const channelSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+      default: "",
+    },
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    subscribers: {
+      type: Number,
+      default: 0,
+    },
+    videos: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Video",
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+const Channel = mongoose.model("Channel", channelSchema);
+
+export default Channel;
