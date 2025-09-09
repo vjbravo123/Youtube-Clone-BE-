d1c81f118d51a4fe1e1b5fdb74f9ce787a327117
vjbravo123 <vjbravojoshi@gmail.com>
2025-09-09 21:26:12 +0530

    add channel routes

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 
 import authRoutes from "./routes/authRoutes.js";
+import channelRoutes from "./routes/channelRoutes.js";
 
 dotenv.config();
 connectDB();
@@ -14,6 +15,7 @@ app.use(express.json());
 
 // Routes
 app.use("/api/auth", authRoutes);
+app.use("/api/channels", channelRoutes);
 
 // Root route
 app.get("/", (req, res) => {
